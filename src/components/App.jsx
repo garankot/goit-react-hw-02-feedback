93529e07116faf767364b7f9571f4e1ed35c3dae
Taras Garan'ko <garanko.t@gmail.com>
2022-01-25 13:07:20 +0200

    components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
-export const App = () => {
-  return <>React homework template</>;
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Please leave Feedback</h2>
+      </div>
+    );
+  }
+}
